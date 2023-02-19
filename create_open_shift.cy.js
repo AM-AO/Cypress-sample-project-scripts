bac92a966f8dae5ee75009e2dd8b359c057f3a76
AAO <106550117+AM-AO@users.noreply.github.com>
2023-02-19 11:44:50 -0600

    Update and rename C2815_create_open_shift.cy.js to create_open_shift.cy.js

--- create_open_shift.cy.js
+++ create_open_shift.cy.js
@@ -1,11 +1,11 @@
 import { LoginPage } from '../page-objects/login_page'
 import { SchedulingPage } from '../page-objects/scheduling_page'
 
-describe('creates open (unassigned) shift', () =>{
+describe('creates open shift', () =>{
     const schedulingPage = new SchedulingPage()
     const loginPage = new LoginPage()
 
-  it('logs in with MAS scheduler user', ()=>{
+  it('logs in with user type 1', ()=>{
       const username = Cypress.env('username')
       const password = Cypress.env('password')
 
@@ -14,32 +14,26 @@ describe('creates open (unassigned) shift', () =>{
       loginPage.validateURL()
   })
 
-  // 1. Click the 'Scheduling' tab
   it('navigates to the scheduling page', () =>{
     schedulingPage.goToScheduling()
   })
 
-  // 2. Click the facility drop down; 3. Select a facility
   it('selects a facility', ()=>{
     schedulingPage.selectFacility()
   })
 
-  // 4. Select 'RN' radio button
-  it('selects RN radio button', ()=>{
+  it('selects employee type', ()=>{
     schedulingPage.setCaregiverType()
   })
 
-  // 5. Navigate to the following week
   it('goes to the next week', ()=>{
     schedulingPage.goNextWeek()
   })
 
-  // 6.Click '+' button for the first shift on the first day of the week
   it('adds an unassigned shift', ()=>{
     schedulingPage.addShift()
   })
 
-  // verifies expected result: an unassigned shift should be created for the correct shift/day
   it('verifies shift was added', ()=>{
     schedulingPage.shiftAppears()
   })
