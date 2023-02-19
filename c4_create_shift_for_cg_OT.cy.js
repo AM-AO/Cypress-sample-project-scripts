7aa0b60194ecad7cac19ccd29bdeb743df8b9059
Alyssa Anderson-Ozhoh <106550117+AM-AO@users.noreply.github.com>
2023-02-19 16:03:47 -0600

    Update c4_create_shift_for_cg_OT.cy.js

--- c4_create_shift_for_cg_OT.cy.js
+++ c4_create_shift_for_cg_OT.cy.js
@@ -1,11 +1,11 @@
 import { LoginPage } from '../page-objects/login_page'
 import { SchedulingPage } from '../page-objects/scheduling_page'
 
-describe('creates a shift for a caregiver who will be in OT', ()=>{
+describe('creates a shift for a caregiver who will be in OT', () => {
     const schedulingPage = new SchedulingPage()
     const loginPage = new LoginPage()
 
-    it('logs in with user type 1', ()=>{
+    it('logs in with user type 1', () => {
         const username = Cypress.env('username')
         const password = Cypress.env('password')
   
@@ -14,27 +14,27 @@ describe('creates a shift for a caregiver who will be in OT', ()=>{
         loginPage.validateURL()
     })
 
-    it('navigates to the scheduling page', () =>{
+    it('navigates to the scheduling page', () => {
         schedulingPage.goToScheduling()
     })
 
-    it ('selects a facility', () =>{
+    it ('selects a facility', () => {
         schedulingPage.selectFacility()
     })
 
-    it('selects an employee', () =>{
+    it('selects an employee', () => {
         schedulingPage.selectEmployee()
     })
 
-    it('goes to the next month', ()=>{
+    it('goes to the next month', () => {
         schedulingPage.goNextMonth()
     })
 
-    it('adds OT shifts', ()=>{
+    it('adds OT shifts', () => {
         schedulingPage.addOtShifts()
     })
 
-    it('deletes OT shifts', ()=>{
+    it('deletes OT shifts', () => {
         schedulingPage.deleteOtshifts()
     })
 })
