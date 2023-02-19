6156ec212d7cd1ab08c02aef50648201f2342504
Alyssa Anderson-Ozhoh <106550117+AM-AO@users.noreply.github.com>
2023-02-19 16:02:50 -0600

    Update c3_create_shift_for_cg.cy.js

--- c3_create_shift_for_cg.cy.js
+++ c3_create_shift_for_cg.cy.js
@@ -1,11 +1,11 @@
 import { LoginPage } from '../page-objects/login_page'
 import { SchedulingPage } from '../page-objects/scheduling_page'
 
-describe('creates a shift for a caregiver', ()=>{
+describe('creates a shift for a caregiver', () => {
     const schedulingPage = new SchedulingPage()
     const loginPage = new LoginPage()
 
-    it('logs in with user type 1', ()=>{
+    it('logs in with user type 1', () => {
         const username = Cypress.env('username')
         const password = Cypress.env('password')
   
@@ -14,31 +14,31 @@ describe('creates a shift for a caregiver', ()=>{
         loginPage.validateURL()
     })
 
-    it('navigates to the scheduling page', () =>{
+    it('navigates to the scheduling page', () => {
         schedulingPage.goToScheduling()
     })
 
-    it('selects a facility', () =>{
+    it('selects a facility', () => {
         schedulingPage.selectFacility()
     })
 
-    it('selects an employee', () =>{
+    it('selects an employee', () => {
         schedulingPage.selectEmployee()
     })
 
-    it('moves two weeks ahead', ()=>{
+    it('moves two weeks ahead', () => {
         schedulingPage.goNextNextWeek()
     })
 
-    it ('adds a shift', ()=>{
+    it ('adds a shift', () => {
         schedulingPage.addShift()
     })
 
-    it ('verifies the shift was added', ()=>{
+    it ('verifies the shift was added', () => {
         cy.get('.MuiAvatar-root').should('be.visible')
     })
 
-    it ('deletes the test shift', ()=>{
+    it ('deletes the test shift', () => {
         schedulingPage.deleteTestShift()
     })
 })
