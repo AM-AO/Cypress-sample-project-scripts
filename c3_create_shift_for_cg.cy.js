045823d4f81affe0d6292f30e2248e64b2fda2e6
AAO <106550117+AM-AO@users.noreply.github.com>
2023-02-19 11:56:04 -0600

    Update and rename C2822_create_shift_for_cg.cy.js to c3_create_shift_for_cg.cy.js

--- c3_create_shift_for_cg.cy.js
+++ c3_create_shift_for_cg.cy.js
@@ -1,4 +1,3 @@
-// import { ConfirmPage } from '../page-objects/confirm_page'
 import { LoginPage } from '../page-objects/login_page'
 import { SchedulingPage } from '../page-objects/scheduling_page'
 
@@ -6,7 +5,7 @@ describe('creates a shift for a caregiver', ()=>{
     const schedulingPage = new SchedulingPage()
     const loginPage = new LoginPage()
 
-    it('logs in with MAS scheduler user', ()=>{
+    it('logs in with user type 1', ()=>{
         const username = Cypress.env('username')
         const password = Cypress.env('password')
   
@@ -15,17 +14,14 @@ describe('creates a shift for a caregiver', ()=>{
         loginPage.validateURL()
     })
 
-    //1. Click the "Scheduling" tab
     it('navigates to the scheduling page', () =>{
         schedulingPage.goToScheduling()
     })
 
-    //2. Click the Facility tab and select a facility
     it('selects a facility', () =>{
         schedulingPage.selectFacility()
     })
 
-    //3. Click the Employee tab and select an employee
     it('selects an employee', () =>{
         schedulingPage.selectEmployee()
     })
@@ -34,12 +30,10 @@ describe('creates a shift for a caregiver', ()=>{
         schedulingPage.goNextNextWeek()
     })
 
-    //4. Select an open day/shift on the calendar; 5.Click the "+" button for that day/shift.
     it ('adds a shift', ()=>{
         schedulingPage.addShift()
     })
 
-    //Verifies the shift was added by asserting that the avitar should be visible
     it ('verifies the shift was added', ()=>{
         cy.get('.MuiAvatar-root').should('be.visible')
     })
