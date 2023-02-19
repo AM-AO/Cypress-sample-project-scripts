b6a7e48055c1e9e855ce41b192ae1f0c7562efa3
AAO <106550117+AM-AO@users.noreply.github.com>
2023-02-19 11:58:04 -0600

    Update and rename C2823_create_shift_for_cg_OT.cy.js to c4_create_shift_for_cg_OT.cy.js

--- c4_create_shift_for_cg_OT.cy.js
+++ c4_create_shift_for_cg_OT.cy.js
@@ -1,4 +1,3 @@
-// import { ConfirmPage } from '../page-objects/confirm_page'
 import { LoginPage } from '../page-objects/login_page'
 import { SchedulingPage } from '../page-objects/scheduling_page'
 
@@ -6,7 +5,7 @@ describe('creates a shift for a caregiver who will be in OT', ()=>{
     const schedulingPage = new SchedulingPage()
     const loginPage = new LoginPage()
 
-    it('logs in with MAS scheduler user', ()=>{
+    it('logs in with user type 1', ()=>{
         const username = Cypress.env('username')
         const password = Cypress.env('password')
   
@@ -15,17 +14,14 @@ describe('creates a shift for a caregiver who will be in OT', ()=>{
         loginPage.validateURL()
     })
 
-    //1. Scheduling
     it('navigates to the scheduling page', () =>{
         schedulingPage.goToScheduling()
     })
 
-    //2. Select facility
     it ('selects a facility', () =>{
         schedulingPage.selectFacility()
     })
 
-    //3. Select employee
     it('selects an employee', () =>{
         schedulingPage.selectEmployee()
     })
@@ -34,8 +30,6 @@ describe('creates a shift for a caregiver who will be in OT', ()=>{
         schedulingPage.goNextMonth()
     })
 
-    //4.Select shift day/time and click "+"; 5.Create 5 shifts and then create a 6th shift; 6.Verify OT modal displays;
-    //7.Select "I do not have an OT document"; 8. Add a note; Click "CONFIRM"
     it('adds OT shifts', ()=>{
         schedulingPage.addOtShifts()
     })
