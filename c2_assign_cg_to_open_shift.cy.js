841704560f13f73ddc219a410e0e7d037d30328a
Alyssa Anderson-Ozhoh <106550117+AM-AO@users.noreply.github.com>
2023-02-19 16:01:46 -0600

    Update c2_assign_cg_to_open_shift.cy.js

--- c2_assign_cg_to_open_shift.cy.js
+++ c2_assign_cg_to_open_shift.cy.js
@@ -2,12 +2,12 @@ import { ConfirmPage } from '../page-objects/confirm_page'
 import { LoginPage } from '../page-objects/login_page'
 import { SchedulingPage } from '../page-objects/scheduling_page'
 
-describe('assigns a caregiver to an open shift', ()=>{
+describe('assigns a caregiver to an open shift', () => {
     const schedulingPage = new SchedulingPage()
     const loginPage = new LoginPage()
     const confirmPage = new ConfirmPage()
 
-    it('logs in with user type 1', ()=>{
+    it('logs in with user type 1', () => {
         const username = Cypress.env('username')
         const password = Cypress.env('password')
   
@@ -16,52 +16,52 @@ describe('assigns a caregiver to an open shift', ()=>{
         loginPage.validateURL()
     })
 
-    it('navigates to the scheduling page', () =>{
+    it('navigates to the scheduling page', () => {
         schedulingPage.goToScheduling()
       })
 
-    it ('selects a facility', () =>{
+    it ('selects a facility', () => {
         schedulingPage.selectFacility()
     })
 
-    it('sets employee type', ()=>{
+    it('sets employee type', () => {
         schedulingPage.setCaregiverType()
     })
 
-    it('goes to next week', ()=>{
+    it('goes to next week', () => {
         schedulingPage.goNextWeek()
     })
 
-    it ('creates an open shift', () =>{
+    it ('creates an open shift', () => {
         schedulingPage.addShift()
     })
 
-    it('verifies shift was added', ()=>{
+    it('verifies shift was added', () => {
         schedulingPage.shiftAppears()
     })
 
-    it('selects an employee', () =>{
+    it('selects an employee', () => {
         schedulingPage.selectEmployee()
     })
 
-    it('views open shifts', () =>{ 
+    it('views open shifts', () => { 
         schedulingPage.viewOpenShifts()
     })
 
-    it('selects the assigned shift', () =>{
+    it('selects the assigned shift', () => {
         schedulingPage.selectOpenShift()
     })
 
-    it ('assigns caregiver to unassigned shift', () =>{
+    it ('assigns caregiver to unassigned shift', () => {
         schedulingPage.assignCaregiver()
     })
 
-    it ('verifies shift is available on confirmations screen', () =>{
+    it ('verifies shift is available on confirmations screen', () => {
         confirmPage.goToConfirm()
         confirmPage.unconfirmedShiftAppears()
     })
 
-    it ('returns to the scheduling page and deletes the test shift', () =>{
+    it ('returns to the scheduling page and deletes the test shift', () => {
         schedulingPage.goToScheduling()
         cy.get('.MuiAvatar-root')
         .each(($ele, index) => {
